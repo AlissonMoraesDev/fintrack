dcafda907b4042312d637e3241356e9c563997bd
Alisson Moraes <alissonmoraesdev@gmail.com>
2025-05-06 22:01:27 -0300

    chore: add shadcn components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Button } from './components/ui/button'
+
 const App = () => {
   return (
     <div>
-      <h1 className="text-red-500">Hello world</h1>
+      <Button>Hello world</Button>
     </div>
   )
 }
