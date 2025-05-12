83990292b0e1d19b369407db6eb87e0060855808
Alisson Moraes <alissonmoraesdev@gmail.com>
2025-05-12 18:26:59 -0300

    feat: add login method to UserService

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -6,7 +6,7 @@ import {
   LOCAL_STORAGE_ACCESS_TOKEN_KEY,
   LOCAL_STORAGE_REFRESH_TOKEN_KEY,
 } from '@/constants/local-storage'
-import { protectedApi, publicApi } from '@/lib/axios'
+import { protectedApi } from '@/lib/axios'
 import { UserService } from '@/services/user'
 
 export const AuthContext = createContext({
@@ -36,7 +36,7 @@ export const AuthContextProvider = ({ children }) => {
   const signupMutation = useMutation({
     mutationKey: ['signup'],
     mutationFn: async (variables) => {
-      const response = UserService.signup(variables)
+      const response = await UserService.signup(variables)
       return response
     },
   })
@@ -44,12 +44,8 @@ export const AuthContextProvider = ({ children }) => {
   const loginMutation = useMutation({
     mutationKey: ['login'],
     mutationFn: async (variables) => {
-      const response = await publicApi.post('/users/login', {
-        email: variables.email,
-        password: variables.password,
-      })
-
-      return response.data
+      const response = await UserService.login(variables)
+      return response
     },
   })
 
