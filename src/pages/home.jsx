3ca44372e5e20e1786ef22879d034b20d9662178
Alisson Moraes <alissonmoraesdev@gmail.com>
2025-05-12 14:10:38 -0300

    feat: add signout

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,10 @@
 import { Navigate } from 'react-router'
 
+import { Button } from '@/components/ui/button'
 import { useAuthContext } from '@/contexts/auth'
 
 const HomePage = () => {
-  const { user, isInitializing } = useAuthContext()
+  const { user, signOut, isInitializing } = useAuthContext()
 
   if (isInitializing) return null
 
@@ -11,10 +12,14 @@ const HomePage = () => {
     return <Navigate to="/login" />
   }
 
+  const handleSignOut = () => {
+    signOut()
+  }
+
   return (
     <div className="flex h-screen w-screen items-start justify-between p-6">
       <h1 className="mb-2 text-2xl font-bold">Dashboard</h1>
-      <div>
+      <div className="flex items-center justify-between">
         <h2 className="text-sm text-white">
           Usuário:{' '}
           <span className="text-sm text-primary-blue">{user.email}</span>
@@ -22,6 +27,7 @@ const HomePage = () => {
         <p className="text-xs text-muted-foreground opacity-90">
           Controle financeiro detalhado.
         </p>
+        <Button onClick={handleSignOut}>Sair</Button>
       </div>
     </div>
   )
