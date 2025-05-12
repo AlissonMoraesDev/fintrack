0b9597229a206ffc3c190f362a4806f6a77e0542
Alisson Moraes <alissonmoraesdev@gmail.com>
2025-05-12 19:17:44 -0300

    feat: add component header in homePage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,10 @@
 import { Navigate } from 'react-router'
 
-import { Button } from '@/components/ui/button'
+import Header from '@/components/header'
 import { useAuthContext } from '@/contexts/auth'
 
 const HomePage = () => {
-  const { user, signOut, isInitializing } = useAuthContext()
+  const { user, isInitializing } = useAuthContext()
 
   if (isInitializing) return null
 
@@ -12,25 +12,7 @@ const HomePage = () => {
     return <Navigate to="/login" />
   }
 
-  const handleSignOut = () => {
-    signOut()
-  }
-
-  return (
-    <div className="flex h-screen w-screen items-start justify-between p-6">
-      <h1 className="mb-2 text-2xl font-bold">Dashboard</h1>
-      <div className="flex flex-col items-end justify-between">
-        <h2 className="text-sm text-white">
-          Usuário:{' '}
-          <span className="text-sm text-primary-blue">{user.email}</span>
-        </h2>
-        <p className="mb-3 text-xs text-muted-foreground opacity-90">
-          Controle financeiro detalhado.
-        </p>
-        <Button onClick={handleSignOut}>Sair</Button>
-      </div>
-    </div>
-  )
+  return <Header />
 }
 
 export default HomePage
